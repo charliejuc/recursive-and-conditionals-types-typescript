4bde657bbe5d6fc992e82bdbc7a1980cb0f3bb02
charliejuc <charliejuc@gmail.com>
2021-03-15 19:36:50 +0100

    Recursive Types, Level Up! => Drop

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-type ParameterByIndex<F extends (...args: never[]) => unknown, Index extends number> = F extends (
-    ...args: infer P
-) => unknown
-    ? P[Index]
-    : never
+type Tail<T extends unknown[]> = T extends [unknown, ...infer Rest] ? Rest : never
+type Head<T extends unknown[]> = T extends [infer H, ...unknown[]] ? H : never
+type Last<T extends unknown[]> = T extends [...unknown[], infer L] ? L : never
 
-const fn = (a: number, b: string, c: string): unknown => 1
+type Drop<Size extends number, T extends unknown[], Indexable extends unknown[] = []> = {
+    continue: Drop<Size, Tail<T>, [...Indexable, unknown]>
+    return: T
+}[Indexable['length'] extends Size ? 'return' : 'continue']
 
-const a: ParameterByIndex<typeof fn, 1> = 'hello'
+type Tuple = [number, string, string[]]
+
+const a: Tail<Tuple> = ['', []]
+const b: Head<Tuple> = 45
+const c: Last<Tuple> = ['dfasf']
+
+const d: Drop<1, Tuple> = ['asdfsa', ['dsfsaf']]
+const e: Drop<2, Tuple> = [['dsfsaf']]
+const f: Drop<3, Tuple> = []
+const g: Drop<4, Tuple>
